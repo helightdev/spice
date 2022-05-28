8090d168514a9e49983c642715aa3ad4800875bb
Helight <helightdev@gmail.com>
2022-05-28 21:30:10 +0200

    More Helpers!

--- lib/src/context.dart
+++ lib/src/context.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 mixin SpiceContextMixin {
 
@@ -8,5 +9,9 @@ mixin SpiceContextMixin {
   TextTheme get text => theme.textTheme;
   ColorScheme get colorScheme => theme.colorScheme;
   MediaQueryData get media => MediaQuery.of(context);
+  double get screenWidth => media.size.width;
+  double get screenHeight => media.size.height;
 
+  BlocBase bloc<B extends BlocBase>() => BlocProvider.of<B>(context);
+  
 }
